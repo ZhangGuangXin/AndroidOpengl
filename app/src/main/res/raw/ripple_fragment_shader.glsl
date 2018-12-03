78d80fe28ad51e443d692a48d084d913402bbb36
ZhangGuangXin <326018204@qq.com>
2018-12-04 00:00:30 +0800

    Update ripple_fragment_shader.glsl

--- app/src/main/res/raw/ripple_fragment_shader.glsl
+++ app/src/main/res/raw/ripple_fragment_shader.glsl
@@ -1,16 +1,50 @@
-precision highp float;
+
+#extension GL_OES_EGL_image_external : require
+precision mediump float;
 
 varying vec2 v_TextureCoordinates;
-uniform sampler2D u_TextureUnit;
-uniform sampler2D a_TextureUnit;
+varying vec4 normalVector;
+varying float isRange;
+uniform samplerExternalOES u_TextureUnit;
+uniform samplerExternalOES a_TextureUnit;
 
 uniform float textureCoordinateY;
 
+uniform float time;
+
+const float step_w = 0.0015625;
+const float step_h = 0.0027778;
+const float maxW   = 0.04;
+
 void main()
 {
+    vec3 lightVector = normalize(vec3(0.0, 4.0, 4.0));
     vec2 textureCoordinate = vec2(v_TextureCoordinates.x, v_TextureCoordinates.y + textureCoordinateY);
-    vec4 TextureFragile = texture2D(u_TextureUnit, textureCoordinate);
-    vec4 TextureWood = texture2D(a_TextureUnit, textureCoordinate);
+//  vec2 textureCoordinate = vec2(v_TextureCoordinates.x, (v_TextureCoordinates.y) + textureCoordinateY);
+//
+//    float x = textureCoordinate.x - maxW*sin((textureCoordinate.y*80.0 - time*7.50));
+//    float x = textureCoordinate.x - maxW*sin((textureCoordinate.y*80.0 ));
+//    x = fract(x);
 
-    gl_FragColor = TextureWood * TextureFragile;
+    vec4 TextureFragile = texture2D(u_TextureUnit, vec2(textureCoordinate.x, textureCoordinate.y));
+    vec4 TextureWood = texture2D(a_TextureUnit, vec2(textureCoordinate.x, textureCoordinate.y));
+
+//    if(isRange == 1.0){
+        gl_FragColor = TextureWood * (max( 0.0, dot(vec3(normalVector.x, normalVector.y, normalVector.z),lightVector)) * 10.0);
+//    }else{
+//        gl_FragColor = TextureWood;
+//    }
+
+
+//    float side = 80.0;
+//        float dotSize = side * 0.25;
+//        vec2 square = vec2(side, side);
+//
+//            vec2 position = mod(gl_FragCoord.xy, square) - square * 0.5;
+//            float length = length(position);
+//            float inside = step(length, dotSize);
+//            vec4 tempColor = vec4(1,0,1,1);
+//            vec4 baseColor = vec4(0,1,0,0);
+//
+//            gl_FragColor = mix(baseColor, tempColor, inside);
 }
